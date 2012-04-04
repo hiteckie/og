ece569ba70a3d2266ae836de5c27a25436b1e292
Teck Chia <teck@teckMBP15.local>
2012-04-04 15:03:29 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Og::Application.config.secret_token = '52c8b1931aa6649bb5519ff506b3f6eb9ac0f99c742c07d0bd30e56f5084bfdeeed0633c441a53760aaddd7d9960f7e7b3092fd077707b83b863514efdb9bc85'
