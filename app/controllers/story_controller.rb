a88506944d32c63f33e0d7a359677e2c1446c013
Teck Chia <teck@navarro-t410s.thefacebook.com>
2012-04-25 13:37:24 -0700

    new

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -84,8 +84,9 @@ class StoryController < ApplicationController
       #pub_id = pub_backend(params['action'], obj, params['url'])
       #og_url += "&content_url=" + params[:content_url]
       og_url = params['og:url']
+      obj_type =  @obj['type']
       logger.info "publish_action OG_URL: " + @obj['type'] + ':' + og_url
-      pub_id = session[:graph_api].put_connections("me", "#{app_namespace}:#{params['og:action']}",' @obj['type'] => og_url)
+      pub_id = session[:graph_api].put_connections("me", "#{app_namespace}:#{params['og:action']}", "#{obj_type}" => og_url)
       logger.info "App namespace: " + app['namespace'] + ", pub_id: " + pub_id.first.to_s
 
     else
