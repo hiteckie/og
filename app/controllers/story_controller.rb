fd9f4607e91a04fe0ab9b105f08e1318ed7c5ae5
Teck Chia <teck@danit-pc.thefacebook.com>
2012-04-26 11:47:09 -0700

    new

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -132,6 +132,9 @@ class StoryController < ApplicationController
       #og_url += "&content_url=" + params[:content_url]
       #og_url = params['og:url']
       obj_type =  @obj['type']
+      if obj_type == 'video.other'
+        obj_type = 'video'
+      end
       logger.info "publish_action OG_URL: " + @obj['type'] + ':' + og_url
       pub_id = session[:graph_api].put_connections("me", "#{params['og:action']}", "#{obj_type}" => og_url)
       logger.info "App namespace: " + app['namespace'] + ", pub_id: " + pub_id.first.to_s
